964e73de30e2086c6e4c5e3c3af0d0b4cb46890f
Jeremiah Tabb <55849254+jtabb1@users.noreply.github.com>
2021-08-21 09:07:10 +0000

    Drafted TreeNode function

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 /* This is in-order traversal for a binary tree */
 
 function TreeNode(value,left,right) {
-  
+  value===undefined ? 0 : value;
+  left===undefined ? null : left;
+  right===undefined ? null : right;
 }
+
+// troot = TreeNode(1,null,TreeNode())
